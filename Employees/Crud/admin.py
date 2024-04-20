c3e3ff7bd66bba78a2002eac3aee4666fe9c20a9
Sarath Chandran <sarath.chandran@ndimensionz.com>
2024-04-20 17:21:11 +0530

    Employe model updated with create and update date and add models to the django admin panel

--- Employees/Crud/admin.py
+++ Employees/Crud/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Employee
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "create_date", "update_date")
+    search_fields = ("name", "email")
+
 
 # Register your models here.
+admin.site.register(Employee, EmployeeAdmin)
--- Employees/Crud/models.py
+++ Employees/Crud/models.py
@@ -1,11 +1,32 @@
 from django.db import models
 
 
+class TimestampeModel(models.Model):
+    """
+    A mixin class add for 'create_date' and 'update_date'
+    """
+
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
 # Create your models here.
-class Employee(models.Model):
+class Employee(TimestampeModel):
+    """
+    Model to represent the Employee
+
+    1. name(str): Name of the employee
+    2. email(str): Email address of the employee
+    3. photo(Imagefiled): To store photo of employee(an optional field)
+
+    """
+
     name = models.CharField(max_length=200)
     email = models.EmailField(unique=True)
     photo = models.ImageField(upload_to="photos/employees", blank=True, null=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.name}({self.email})"
